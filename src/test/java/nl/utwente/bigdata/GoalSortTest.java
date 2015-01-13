dc20ef54a386f3be4b651a7cdc241e0890fdf964
Thom Ritterfeld <thom@ritterfeld.com>
2015-01-13 14:38:16 +0100

    Finished GoalSort and tests

--- src/test/java/nl/utwente/bigdata/GoalSortTest.java
+++ src/test/java/nl/utwente/bigdata/GoalSortTest.java
@@ -1,14 +1,20 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.binding.ListBinding;
 import nl.utwente.bigdata.GoalSort;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
  
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,51 +23,46 @@ import org.junit.Test;
 public class GoalSortTest {
  
   private MapDriver<Object, Text, LongWritable, Text> mapDriver;
-  private ReduceDriver<LongWritable, Text, LongWritable, ArrayWritable> reduceDriver;
-  private MapReduceDriver<Object, Text, LongWritable, Text, LongWritable, ArrayWritable> mapReduceDriver; 
- 
+
   @Before
   public void setUp() {
-    GoalSort.TimestampMapper mapper   = new GoalSort.TimestampMapper();
-//    GoalSort.GoalReducer reducer = new GoalSort.GoalReducer();
+    GoalSort.GoalMapper mapper   = new GoalSort.GoalMapper();
     mapDriver = MapDriver.newMapDriver(mapper);
-//    reduceDriver = ReduceDriver.newReduceDriver(reducer);
-//    mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
   }
- 
-
+  
+  @Test
+    public void testNoGoal() {
+        List<Pair<LongWritable, Text>> out = null;
+        try {
+            Object key = new Object();
+            Text value = new Text("{\"filter_level\":\"medium\",\"contributors\":null,\"text\":\"Hello world my country did not score!\",\"id_str\":\"1\", \"user\":{\"id_str\":\"1\"},\"created_at\":\"Thu Jul 03 05:17:20 +0000 2014\"}");
+            out = mapDriver.withInput(key, value).run();
+            assert(out.isEmpty());
+        } catch (IOException ioe) {
+            //Failed 
+            assert(false);
+        }
+        assert(out != null);
+    }
     
     @Test
-    public void testMapper() {
+    public void testGoalWithKeyword() {
         Object key = new Object();
-        Text value = new Text("{\"filter_level\":\"medium\",\"contributors\":null,\"text\":\"Hello world!\",\"id_str\":\"1\", \"user\":{\"id_str\":\"1\"},\"created_at\":\"Thu Jul 03 05:17:20 +0000 2014\"}");
+        Text value = new Text("{\"filter_level\":\"medium\",\"contributors\":null,\"text\":\"Hello world my country scored a Japenese ゴール!\",\"id_str\":\"1\", \"user\":{\"id_str\":\"1\"},\"created_at\":\"Thu Jul 03 05:17:20 +0000 2014\"}");
         mapDriver.withInput(key, value);
         long tweetTimestamp = Long.valueOf("1404364640000");
-        mapDriver.withOutput(new LongWritable(tweetTimestamp), new Text("Hello world!"));
+        mapDriver.withOutput(new LongWritable(tweetTimestamp), new Text("Hello world my country scored a Japenese ゴール!"));
         mapDriver.runTest();
     }
- 
-
-//  @Test
-//  public void testReducer() {
-//    List<Text> values = new ArrayList<Text>();
-//    values.add(new Text("A simple tweet"));
-//    values.add(new Text("Hello world!"));
-//    String[] texts = {"A simple tweet", "Hello world!"};
-//    reduceDriver.withInput(new LongWritable(1404364640), values);
-//    reduceDriver.withOutput(new LongWritable(1404364640), new ArrayWritable(texts));
-//    reduceDriver.runTest();
-//  }
-
-
-//  @Test
-//  public void testMapReduce() {
-//    Object key = new Object();
-//    Text value = new Text("{\"filter_level\":\"medium\",\"contributors\":null,\"text\":\"A nice tweet indeed\",\"id_str\":\"1\"}");
-//    mapReduceDriver.withInput(key, value);
-//    String[] texts = {"A nice tweet indeed"};
-//    mapReduceDriver.withOutput(new LongWritable(1404364640), new ArrayWritable(texts));
-//    mapReduceDriver.runTest();
-//  }
+    
+    @Test
+    public void testGoalWithScore() {
+        Object key = new Object();
+        Text value = new Text("{\"filter_level\":\"medium\",\"contributors\":null,\"text\":\"Hello world my country scored 1-1!\",\"id_str\":\"1\", \"user\":{\"id_str\":\"1\"},\"created_at\":\"Thu Jul 03 05:17:20 +0000 2014\"}");
+        mapDriver.withInput(key, value);
+        long tweetTimestamp = Long.valueOf("1404364640000");
+        mapDriver.withOutput(new LongWritable(tweetTimestamp), new Text("Hello world my country scored 1-1!"));
+        mapDriver.runTest();
+    }
 
 }
